31739778de96ab968baf1dcb99a2b577db1bb586
Ian Harris <ian+gh@dgby.org>
2016-04-18 03:50:08 -0700

    E1-13 (and friends)

---
+++ 1/13.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+#define CHAR_LOW 33
+#define CHAR_HIGH 126
+
+main() {
+    int i, j;
+    int ndigit[CHAR_HIGH + 1];
+    char c;
+
+    for (i = 0; i <= CHAR_HIGH + 1; i++) {
+        ndigit[i] = 0;
+    }
+
+    while ((c = getchar()) != EOF) {
+        if (c >= CHAR_LOW && c <= CHAR_HIGH) {
+            ndigit[c]++;
+        }
+    }
+
+    printf("output:\n");
+    for (i = CHAR_LOW; i <= CHAR_HIGH; i++) {
+//        if (ndigit[i] == 0) {
+  //          continue;
+    //    }
+        printf("%c: ", i);
+        for (j = 1; j <= ndigit[i]; j++) {
+            putchar('*');
+        }
+        putchar('\n');
+    }
+    return 0;
+}
