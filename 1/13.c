2d4b234cdec98ac0d5be0534e81c6b5c224011e7
Ian Harris <ian+gh@dgby.org>
2016-04-29 22:19:09 -0700

    E1-13 (fix)

--- 1/13.c
+++ 1/13.c
@@ -1,33 +1,40 @@
 #include <stdio.h>
 
-#define CHAR_LOW 33
-#define CHAR_HIGH 126
+#define IN 1
+#define OUT 0
+#define MAX_WORD 1000
 
 main() {
-    int i, j;
-    int ndigit[CHAR_HIGH + 1];
-    char c;
+    int c, nw, lw, state;
+    // curChar, numWords, lenWord, state;
+    int nword[MAX_WORD + 1];
 
-    for (i = 0; i <= CHAR_HIGH + 1; i++) {
-        ndigit[i] = 0;
+    state = OUT;
+    nw = lw = 0;
+
+    for (int i = 0; i <= MAX_WORD + 1; i++) {
+        nword[i] = 0;
     }
 
     while ((c = getchar()) != EOF) {
-        if (c >= CHAR_LOW && c <= CHAR_HIGH) {
-            ndigit[c]++;
+        if (c == ' ' || c == '\n' || c == '\t') {
+            state = OUT;
+            nword[lw]++;
+            lw = 0;
+        } else {
+            state = IN;
+            ++lw;
         }
     }
-
     printf("output:\n");
-    for (i = CHAR_LOW; i <= CHAR_HIGH; i++) {
-//        if (ndigit[i] == 0) {
-  //          continue;
-    //    }
-        printf("%c: ", i);
-        for (j = 1; j <= ndigit[i]; j++) {
-            putchar('*');
+    for (int i = 1; i <= MAX_WORD; i++) {
+        if (nword[i] > 0) {
+            printf("length %3d: ", i);
+            for (int j = 1; j <= nword[i]; j++) {
+                putchar('*');
+            }
+            putchar('\n');
         }
-        putchar('\n');
     }
     return 0;
 }
